bf894d5b6ab8c36bd0bb986adffa3162bb417a4f
Nicole Lewey <lewe4441@pacificu.edu>
2014-11-12 12:48:51 -0800

    Added character class and unit test skeletons

--- Character.py
+++ Character.py
@@ -4,16 +4,14 @@
 # File Name: 	NinjaGame.py
 # Authors: 		Nicole Lewey and Jacob Lundgren
 # Date: 		12/08/2014
-# Class:		CS360 - Open Source
-# Assignment:	Ninja Game - Create Open Source Project
+# Class: 		CS360 - Open Source
+# Assignment: 	Ninja Game - Create Open Source Project
 # Purpose: 		Main for the Ninja Game
 ########################################################################
 
-from Character import *
+import pygame
 
-def main():
-	ninja = Character()
-
-# invoke main()
-if __name__=="__main__":
-	main()
+class Character(pygame.sprite.Sprite):
+	
+	def __init__(self):
+		self.x = 5
--- CharacterUnitTests.py
+++ CharacterUnitTests.py
@@ -4,16 +4,7 @@
 # File Name: 	NinjaGame.py
 # Authors: 		Nicole Lewey and Jacob Lundgren
 # Date: 		12/08/2014
-# Class:		CS360 - Open Source
-# Assignment:	Ninja Game - Create Open Source Project
+# Class: 		CS360 - Open Source
+# Assignment: 	Ninja Game - Create Open Source Project
 # Purpose: 		Main for the Ninja Game
 ########################################################################
-
-from Character import *
-
-def main():
-	ninja = Character()
-
-# invoke main()
-if __name__=="__main__":
-	main()
