fff8a1aa2a5a3892eb9aefd7b828d34125f8c9b7
root <root@cs360-6.cs.pacificu.edu>
2014-11-16 16:26:11 -0800

    update

--- NinjaGame.py
+++ NinjaGame.py
@@ -9,9 +9,24 @@
 # Purpose: Main for the Ninja Game
 ########################################################################
 
+import pygame
+from Wall import *
+from WallGroup import *
+from pygame.locals import *
+
+SCREEN_LENGTH = 900
+SCREEN_WIDTH = 600
 
 def main():
-	#stuff
+	pygame.init()
+	screen = pygame.display.set_mode ((SCREEN_LENGTH, SCREEN_WIDTH))	
+	walls = WallGroup(screen)
+	walls.draw (screen)
+	pygame.display.update()
+	
+	while True:
+		if pygame.event.poll().type is QUIT:
+			break
 
 # invoke main()
 if __name__=="__main__" :
